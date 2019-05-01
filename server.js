60ee31784b4b3efe52281f4ad3ca2e83327d849e
chibuzor christian <gigzblog@gmail.com>
2019-05-01 17:14:48 +0100

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const port = 3000
-app.get('/', (req, res) => res.send('Welcome to  widget dev'))
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/dist'));
+app.get('/', (req, res) => res.send('Welcome to  widget dev'))
 app.get('/index.html', (req, res) => {
     res.sendFile('index.html', {
         root: path.join(__dirname, './')
     })
 })
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+    });
