3598f2ac17e33cc67aba152f1ace90b45a97f385
chibuzor christian <gigzblog@gmail.com>
2019-05-04 00:06:40 +0100

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-//app.set('port', (process.env.PORT || 3000));
-app.set('port',8000);
+app.set('port', (process.env.PORT || 3000));
+//app.set('port',8000);
 app.use(express.static(__dirname + '/dist'));
 app.get('/', (req, res) => res.send('Welcome to  widget dev'))
 app.get('/index.html', (req, res) => {
